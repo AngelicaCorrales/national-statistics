c327b821601d9dfee1fac3b21714fa9ff298b745
KerenLopez <kerenlopezcordoba@outlook.es>
2022-01-28 12:16:20 -0500

    NationalStatistics class was created

---
+++ national-statistics/src/model/NationalStatistics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace national_statistics.src.model
+{
+    public class NationalStatistics { 
+
+        //Relationships
+          private List<Department> departments;
+    
+        //Builder method
+        public NationalStatistics()
+        {
+            departments = new List<Department>();
+        }
+
+        public importFile()
+        {
+            
+        }
+
+        public filterDepartments()
+        {
+
+        }
+
+
+
+    }
+}
