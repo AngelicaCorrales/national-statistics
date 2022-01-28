9b505398ec0e4e2f166f4782431897870089c73e
KerenLopez <kerenlopezcordoba@outlook.es>
2022-01-28 11:14:13 -0500

    Changes in municipality class: attributes and new methods added

--- national-statistics/src/model/Municipality.cs
+++ national-statistics/src/model/Municipality.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace national_statistics.src.model
 {
-    internal class Municipality
+    public class Municipality
     {
+        // Attributes
+     
+        private string code;
+        private string name;
+        private string type;
+
+        // Builder method
+        public Municipality(string c, string n, string t)
+        {
+            this.code = c;
+            this.name = n;
+            this.type = t;
+        }
+
+        public string getCode() 
+        { 
+            return code; 
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getType()
+        {
+            return type;
+        }
+
     }
 }
